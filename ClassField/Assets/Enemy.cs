459660cba84bcadbd3941d66ba1c875e809164d0
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-04-08 16:32:48 +0800

    課堂練習01

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("血量"), Tooltip("怪物的血量"), Range(50,500)]
+    public int health;
+    [Header("魔力"), Tooltip("怪物的魔力"), Range(10, 100)]
+    public int mana = 100;
+    [Header("能力值"), Tooltip("怪物的能力值"), Range(1,50.5f)]
+    public float speed = 10.5f;
+    [Header("攻擊力"), Tooltip("怪物的攻擊力"), Range(20, 200)]
+    public float attack = 50;
+    [Header("防禦力"), Tooltip("怪物的防禦力"), Range(0, 100)]
+    public float defense;
+    [Header("裝備")]
+    public string weapon;
+    public string equipment = "皮衣";
+    [Header("其他資料"), Tooltip("怪物是否帶有鑰匙")]
+    public bool key;
+    [Tooltip("怪物是否帶有寶物")]
+    public bool treasure;
+
+
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物一號")]
+    public Enemy enemy1;
+    [Header("怪物二號")]
+    public Enemy enemy2;
+
+    private void Start()
+    {
+        print("怪物一號的魔力" + enemy1.mana);
+        print("怪物二號的裝備名稱" + enemy2.equipment);
+
+        enemy1.health = 100;
+        enemy2.health = 300;
+        enemy1.defense = 50;
+        enemy1.weapon = "木棍";
+        enemy2.weapon = "短刀";
+        enemy1.key = true;
+        enemy2.treasure = true;
+    }
+}
